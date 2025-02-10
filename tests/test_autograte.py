993db741f62646182dddd32fd403338cdc788771
Donghwan Shin <d.shin@sheffield.ac.uk>
2025-02-10 17:52:22 +0000

    Update test_autograte.py

--- tests/test_autograte.py
+++ tests/test_autograte.py
@@ -5,5 +5,5 @@ import hello
 class TestAutograder(unittest.TestCase):
     def test_result(self):
         # check if the file `causal_testing.png` exists
-        self.assertEqual("Hello World!", hello.say_hello())
+        self.assertEqual("Hello Reengineering", hello.say_hello())
 
